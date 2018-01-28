db76a4bacee7cffab93edbcfb5fafdac2fbb1df4
Vishesh <visheshsingh16@gmail.com>
2018-01-27 18:33:04 -0600

    Map Java class to DB table

---
+++ src/com/visheshthakur/hibernate/entity/Student.java
@@ -0,0 +1,13 @@
+package com.visheshthakur.hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+	public Student() {
+
+	}
+}
